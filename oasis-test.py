22fa8db5dc02cbd97c6527c6a7c16338a1232242
Dylan Dinio <dylan@Dylans-MacBook-Pro.local>
2021-10-17 16:17:57 -0400

    First commit

---
+++ oasis-test.py
@@ -0,0 +1,11 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import time
+
+driver = webdriver.Chrome()
+driver.get("https://nudining.com/public/menus")
+time.sleep(10)
+elem = driver.find_elements_by_css_selector('[data-label="Menu item"]')
+print(elem)
+
+# driver.close()
